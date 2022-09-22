1ec809a1cdce84c805461e3e3d8b6c62f51ba988
harrisimon <harrisimon@gmail.com>
2022-09-22 10:33:14 -0400

    updated rat imgs for directional movement

--- js/rat.js
+++ js/rat.js
@@ -13,6 +13,11 @@ export default class Rat {
     this.down = false,
     this.right = false,
     this.left = false
+    this.currCell = {
+        x: null,
+        y: null
+    }
+    this.ratPicIndex = 0
 
     document.addEventListener("keydown", this.setDirection)
     document.addEventListener("keyup", this.unsetDirection)
@@ -33,32 +38,35 @@ export default class Rat {
 
   #loadRatPics() {
     const ratPic1 = new Image()
-    ratPic1.src = "../imgs/rat1.png"
+    ratPic1.src = "../imgs/ratRight.png"
     const ratPic2 = new Image()
-    ratPic2.src = "../imgs/rat1.png"
+    ratPic2.src = "../imgs/ratDown.png"
     const ratPic3 = new Image()
-    ratPic3.src = "../imgs/rat3.png"
+    ratPic3.src = "../imgs/ratLeft.png"
+    const ratPic4 = new Image ()
+    ratPic4.src = '../imgs/ratUp.png'
+    this.ratPicArray = [ratPic1, ratPic2, ratPic3, ratPic4]
 
-    this.ratPicArray = [ratPic1, ratPic2, ratPic3, ratPic2]
-
-    this.ratPicIndex = 0
+    // this.ratPicIndex = 0
   }
 
   //   }
   setDirection = (event) => {
     if (event.key === "ArrowRight") {
-    
-      this.keys.push(event.key)
       this.requestedMovingDirection = 'ArrowRight'
+      this.ratPicIndex = 0
     } else if (event.key === "ArrowLeft") {
       this.keys.push(event.key)
-      this.requestedMovingDirection = this.keys.at(-1)
+      this.requestedMovingDirection = 'ArrowLeft'
+      this.ratPicIndex = 2
     } else if (event.key === "ArrowUp") {
       this.keys.push(event.key)
-      this.requestedMovingDirection = this.keys.at(-1)
+      this.requestedMovingDirection = 'ArrowUp'
+      this.ratPicIndex = 3
     } else if (event.key === "ArrowDown") {
       this.keys.push(event.key)
-      this.requestedMovingDirection = this.keys.at(-1)
+      this.ratPicIndex = 1
+      this.requestedMovingDirection = 'ArrowDown'
     }
   }
   unsetDirection = () => {
@@ -66,18 +74,27 @@ export default class Rat {
     if (this.requestedMovingDirection === 'ArrowRight'){
         this.x+=32
         this.requestedMovingDirection = null
+        this.currCell.x = this.x
+        this.currCell.y = this.y 
+        // console.log(this.currCell)
     //if not blocked
     } else if (this.requestedMovingDirection === 'ArrowLeft'){
         this.x-=32
         this.requestedMovingDirection = null
+        this.currCell.x = this.x
+        this.currCell.y = this.y 
     //if not blocked
     } else  if (this.requestedMovingDirection === 'ArrowDown'){
         this.y+=32
         this.requestedMovingDirection = null
+        this.currCell.x = this.x
+        this.currCell.y = this.y 
     //if not blocked
     } else if (this.requestedMovingDirection === 'ArrowUp'){
         this.y-=32
         this.requestedMovingDirection = null
+        this.currCell.x = this.x
+        this.currCell.y = this.y 
     }
     // if (this.keys.length >= 0) {
     // // this.speed = 0
